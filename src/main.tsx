d42ae8140caf1d9cb9c54da122ce81d547fc20a5
WAULEX01 <WAULEX2014@GMAIL.COM>
2026-08-26 10:48:55 -0400

    perf: ativar otimização gráfica segura no mobile

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/multiplica-typeui.css'
 import './styles/v823-production-layout.css'
+import './styles/mobile-performance-v823.css'
 import App from './App.tsx'
 
 // Aplica o tema salvo (dia/noite) antes do React montar, evitando flash de tema errado
